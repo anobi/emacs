c688f3e1533a4050b12faedefd3bccda5d7cb23e
Niko Salakka <niko.salakka@yle.fi>
2025-06-24 13:26:43 +0300

    org-roam

--- lang/org.el
+++ lang/org.el
@@ -5,18 +5,38 @@
 
 ;;; Code:
 
-(require 'org)
 
-(add-hook 'org-mode-hook 'org-indent-mode)
+(use-package org
+  :mode (("\\.org$" . org-mode))
+  :ensure nil  ;; Use the built-in org mode
+  :hook (org-mode-hook . org-indent-mode)
+  :config
+    (setq org-todo-keywords '((sequence "TODO" "PLANNING" "DOING" "REVIEW" "ON HOLD" "|" "DONE")))
+    (setq org-todo-keyword-faces
+        '(("TODO" . org-warning)
+        ("PLANNING" "green yellow")
+        ("DOING" . org-agenda-clocking)
+        ("REVIEW" . "yellow")
+        ("ON HOLD" . "dim grey")
+        ("DONE" . org-done))))
 
-(setq org-todo-keywords '((sequence "TODO" "PLANNING" "DOING" "REVIEW" "ON HOLD" "|" "DONE")))
-(setq org-todo-keyword-faces
-	  '(("TODO" . org-warning)
-    ("PLANNING" "green yellow")
-		("DOING" . org-agenda-clocking)
-		("REVIEW" . "yellow")
-    ("ON HOLD" . "dim grey")
-		("DONE" . org-done)))
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/org-notes/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 
 (provide 'org.el)
 ;;; org.el ends here
--- minibuffer.el
+++ minibuffer.el
@@ -6,76 +6,88 @@
 ;;; Code:
 
 
-(defvar minibuffer-packages '(
-  vertico
-  marginalia
-  orderless
-  consult
-  embark
-  embark-consult
-))
-
-(dolist (package minibuffer-packages)
-    (unless (package-installed-p package)
-        (package-install package)))
-
-
 ;; Enable and configure Vertico
 (use-package vertico
   :init
   (vertico-mode))
 
+
 (use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
   :bind (:map minibuffer-local-map
-              ("M-A" . marginalia-cycle))
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
   :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
   (marginalia-mode))
+
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
 
 (use-package consult
   :ensure t
-
   :hook
   (completion-list-mode . consult-preview-at-point-mode)
-
   :init
   (setq register-preview-delay 0.5
         register-preview-function #'consult-register-window)
   (advice-add #'register-preview :override #'consult-register-window)
 
   (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-funtion #'consult-xref)
+        xref-show-definitions-funtion #'consult-xref))
 
-  :config)
-  
 
 (use-package embark
   :ensure t
-  
   :bind
   (("C-." . embark-act)
    ("C-;" . embark-dwim)
    ("C-h B" . embark-bindings))
-
   :init
   (setq prefix-help-command #'embark-prefix-help-command)
-
   :config
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*'"
                  nil
                  (window-parameters (mode-line-format . none)))))
 
+
 (use-package embark-consult
   :ensure t
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
-      ;; Enable and configure Orderless
-(use-package orderless
-  :ensure t
+
+;; Emacs minibuffer configurations.
+(use-package emacs
+  :ensure nil
   :custom
-  (completion-styles '(substring orderless basic))
-  (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion)))))
+  ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+  ;; to switch display modes.
+  (context-menu-mode t)
+  ;; Support opening new minibuffers from inside existing minibuffers.
+  (enable-recursive-minibuffers t)
+  ;; Hide commands in M-x which do not work in the current mode.  Vertico
+  ;; commands are hidden in normal buffers. This setting is useful beyond
+  ;; Vertico.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; Do not allow the cursor in the minibuffer prompt
+  (minibuffer-prompt-properties
+   '(read-only t cursor-intangible t face minibuffer-prompt)))
    
 ;;; minibuffer.el ends here
